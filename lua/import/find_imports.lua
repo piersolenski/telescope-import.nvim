bcd39a26b65b14bbdba4bf6aee38f1800b3b256c
Piers Olenski <hello@piers.io>
2024-03-08 17:42:47 -0600

    Add removing of existing import statements

--- lua/import/find_imports.lua
+++ lua/import/find_imports.lua
@@ -23,6 +23,36 @@ local function get_filetype_config(languages, filetype)
   return nil
 end
 
+-- Find the imports in the current file
+local function find_current_file_imports(languages)
+  local filetype = utils.get_filetype()
+  local config = get_filetype_config(languages, filetype)
+
+  if config == nil then
+    return nil
+  end
+
+  local types = create_file_types_flag(config.extensions)
+  local flags = { "--no-heading", "--no-line-number", "--color=never", "--no-filename" }
+
+  local current_file_path = vim.api.nvim_buf_get_name(0)
+
+  local find_command = "rg "
+    .. types
+    .. " "
+    .. table.concat(flags, " ")
+    .. " "
+    .. '"'
+    .. config.regex
+    .. '"'
+    .. " "
+    .. current_file_path
+
+  local results = vim.fn.systemlist(find_command)
+
+  return results
+end
+
 local function find_imports(languages)
   local filetype = utils.get_filetype()
   local config = get_filetype_config(languages, filetype)
@@ -45,8 +75,11 @@ local function find_imports(languages)
 
   local results = vim.fn.systemlist(find_command)
 
+  local local_results = find_current_file_imports(languages)
+
   results = utils.sort_by_frequency(results)
   results = utils.remove_duplicates(results)
+  results = utils.remove_entries(results, local_results)
 
   local imports = {}
 
