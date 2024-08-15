efd4932fac79b758855e10e17c6bcf8e5671d96e
Piers Olenski <hello@piers.io>
2024-08-15 21:16:49 +0100

    Main implementation

--- lua/import/insert_line.lua
+++ lua/import/insert_line.lua
@@ -1,14 +1,37 @@
-local function insert_line(value, insert_at_top)
+local function insert_line(value, line_number)
+  -- Capture the original line so that we can return to it if we insert the line
+  -- elsewhere
   local original_position
 
-  if insert_at_top then
+  if line_number then
+    -- If the line_number is a function, call it to get the resulting number
+    if type(line_number) == "function" then
+      line_number = line_number()
+    end
+
+    -- Double check that we have a number in the case of custom configs
+    if type(line_number) ~= "number" then
+      error("Expected insert_at_line to return a number, but got " .. type(line_number))
+    end
+
     original_position = vim.fn.getpos(".")
-    vim.cmd("normal! gg")
+
+    -- Ensure the line number is within the valid range
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if line_number < 1 or line_number > line_count then
+      error("Line number out of range!")
+      return
+    end
+    -- Set cursor to the desired line
+    vim.api.nvim_win_set_cursor(0, { line_number, 0 })
   end
 
+  -- Insert the line
   vim.api.nvim_put({ value }, "l", false, false)
 
-  if insert_at_top then
+  if line_number then
+    -- Increment the line number to accommodate for extra line
+    original_position[2] = original_position[2] + 1
     vim.fn.setpos(".", original_position)
   end
 end
--- lua/import/picker.lua
+++ lua/import/picker.lua
@@ -1,17 +1,19 @@
-local actions = require("telescope.actions")
-local finders = require("telescope.finders")
-local pickers = require("telescope.pickers")
-local conf = require("telescope.config").values
 local action_state = require("telescope.actions.state")
-local utils = require("import.utils")
+local actions = require("telescope.actions")
+local conf = require("telescope.config").values
 local default_languages = require("import.languages")
+local finders = require("telescope.finders")
+local get_filetype_config = require("import.get_filetype_config")
 local get_project_imports = require("import.get_project_imports")
 local insert_line = require("import.insert_line")
+local pickers = require("telescope.pickers")
+local utils = require("import.utils")
 
 local function picker(opts)
   local languages = utils.concat_tables(opts.custom_languages, default_languages)
-
-  local imports = get_project_imports(languages)
+  local filetype = utils.get_filetype()
+  local filetype_config = get_filetype_config(languages, filetype)
+  local imports = get_project_imports(filetype_config)
 
   if imports == nil then
     vim.notify("Filetype not supported", vim.log.levels.ERROR)
@@ -56,8 +58,11 @@ local function picker(opts)
       attach_mappings = function(prompt_bufnr, _)
         actions.select_default:replace(function()
           actions.close(prompt_bufnr)
+          local insert_line_at = (
+            filetype_config and filetype_config.insert_at_line or opts.insert_at_line
+          )
           local selection = action_state.get_selected_entry()
-          insert_line(selection.value, opts.insert_at_top)
+          insert_line(selection.value, insert_line_at)
         end)
         return true
       end,
