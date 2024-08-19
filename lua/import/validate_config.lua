05a5d8175ab64655bbb1750c5fc919e085427f49
Piers Olenski <hello@piers.io>
2024-08-19 15:14:33 +0100

    Improve validation

--- lua/import/validate_config.lua
+++ lua/import/validate_config.lua
@@ -1,45 +1,42 @@
+local function validate_language(lang)
+  vim.validate({
+    extensions = { lang.extensions, "table" },
+    filetypes = { lang.filetypes, "table" },
+    regex = { lang.regex, "string" },
+    insert_at_line = { lang.insert_at_line, { "nil", "number", "function" }, true },
+  })
+
+  for _, ext in ipairs(lang.extensions) do
+    vim.validate({ extension = { ext, "string" } })
+  end
+
+  for _, ft in ipairs(lang.filetypes) do
+    vim.validate({ filetype = { ft, "string" } })
+  end
+end
+
 local function validate_config(opts)
-  return vim.validate({
-    insert_at_line = { opts.insert_at_line, { "nil", "number", "function" } },
+  vim.validate({
+    insert_at_top = { opts.insert_at_top, { "boolean" } },
     custom_languages = {
       opts.custom_languages,
       function(value)
         if type(value) ~= "table" and value ~= nil then
           return false, "custom_languages must be a table or nil"
         end
-        for _, lang in ipairs(value or {}) do
-          if type(lang) ~= "table" then
-            return false, "each item in custom_languages must be a table"
-          end
-          -- Ensure that extensions, filetypes, and regex are always present and correctly formatted
-          if
-            not (
-              type(lang.extensions) == "table"
-              and type(lang.filetypes) == "table"
-              and type(lang.regex) == "string"
-            )
-          then
-            return false,
-              "extensions, filetypes, and regex are all required fields with proper structure in each language table"
-          end
 
-          for _, ext in ipairs(lang.extensions) do
-            if type(ext) ~= "string" then
-              return false, "all extensions should be strings"
+        if value ~= nil then
+          for _, lang in ipairs(value) do
+            if type(lang) ~= "table" then
+              return false, "each item in custom_languages must be a table"
             end
-          end
-
-          for _, ft in ipairs(lang.filetypes) do
-            if type(ft) ~= "string" then
-              return false, "all filetypes should be strings"
+            local ok, err = pcall(validate_language, lang)
+            if not ok then
+              return false, err
             end
-          end
-
-          -- Optional validation for insert_at_line
-          if lang.insert_at_line and type(lang.insert_at_line) ~= "number" then
-            return false, "insert_at_line must be a number if provided"
           end
         end
+
         return true
       end,
     },
