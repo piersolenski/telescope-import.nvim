729419e9a4a864e5a91792086f5050ac439f2ee0
Piers Olenski <1285419+piersolenski@users.noreply.github.com>
2025-07-14 17:27:44 +0100

    Support legacy setup (#43)

--- lua/import/config.lua
+++ lua/import/config.lua
@@ -4,16 +4,16 @@ local M = {}
 
 M.options = {}
 
+M.default_options = {
+  custom_languages = {},
+  insert_at_top = true,
+  picker = "telescope",
+}
+
 M.setup = function(opts)
   opts = opts or {}
 
-  local default_opts = {
-    custom_languages = {},
-    insert_at_top = true,
-    picker = "telescope",
-  }
-
-  local merged_opts = vim.tbl_deep_extend("force", default_opts, opts)
+  local merged_opts = vim.tbl_deep_extend("force", M.default_options, opts)
 
   validate_config(merged_opts)
 
